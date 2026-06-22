42870d3f0843190ef2386a33fd4bda101c0959dc
Sunday <sunnexajayi@gmail.com>
2026-06-22 19:38:42 +0200

    feat: add database synchronization script and update gitignore for script management

---
+++ scripts/sync-db.js
@@ -0,0 +1,29 @@
+import { connectDB } from "../config/db.js";
+import sequelize from "../config/db.js";
+import User from "../models/User.js";
+import { setupCourseAssociations } from "../models/Course.js";
+import "../models/Module.js";
+import "../models/Lesson.js";
+import "../models/Quiz.js";
+import "../models/Enrollment.js";
+import "../models/LessonProgress.js";
+import "../models/Review.js";
+import "../models/Video.js";
+
+// Make sure associations are set up before syncing
+setupCourseAssociations(User);
+
+async function runSync() {
+  try {
+    console.log("Starting database sync...");
+    await connectDB();
+    await sequelize.sync({ alter: true });
+    console.log("Database schema synced successfully via CI/CD!");
+    process.exit(0);
+  } catch (error) {
+    console.error("Failed to sync database schema:", error);
+    process.exit(1);
+  }
+}
+
+runSync();
