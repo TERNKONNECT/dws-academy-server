b345096d1d2f1b859ff1375574bcbf3e8163ad80
Sunday <sunnexajayi@gmail.com>
2026-06-23 11:05:22 +0200

    chore: migrate from aws-sdk v2 to AWS SDK for JavaScript v3 S3 client

--- config/s3.js
+++ config/s3.js
@@ -1,8 +1,7 @@
-import AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
 
 const s3Config = {
   region: process.env.AWS_S3_REGION || process.env.AWS_REGION,
-  signatureVersion: "v4",
 };
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_ID;
@@ -10,13 +9,15 @@ const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || process.env.AWS_ACC
 const sessionToken = process.env.AWS_SESSION_TOKEN;
 
 if (accessKeyId && secretAccessKey) {
-  s3Config.accessKeyId = accessKeyId;
-  s3Config.secretAccessKey = secretAccessKey;
+  s3Config.credentials = {
+    accessKeyId,
+    secretAccessKey,
+  };
   if (sessionToken) {
-    s3Config.sessionToken = sessionToken;
+    s3Config.credentials.sessionToken = sessionToken;
   }
 }
 
-const s3 = new AWS.S3(s3Config);
+const s3 = new S3Client(s3Config);
 
 export default s3;
